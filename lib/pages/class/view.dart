1f95d50c35b9158e92eefc9bb270b378699dd986
Just-Prog <JustProg5835@outlook.com>
2025-03-27 10:04:56 +0800

    优化班级内容组件结构

--- lib/pages/class/view.dart
+++ lib/pages/class/view.dart
@@ -71,126 +71,126 @@ class _ClassState extends State<ClassPage> with TickerProviderStateMixin {
         return TabBarView(
           controller: _tabController,
           children: [
-            CustomScrollView(
-              physics: const NeverScrollableScrollPhysics(),
-              slivers: [
-                SliverOverlapInjector(
-                  handle: NestedScrollView.sliverOverlapAbsorberHandleFor(ctx),
-                ),
-                SliverToBoxAdapter(
-                    child: Obx(() => Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: _classController.chaptersList
-                              .map((e) => Padding(
-                                  padding: EdgeInsets.symmetric(horizontal: 10),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Padding(
-                                        padding: EdgeInsets.symmetric(
-                                            vertical: 15, horizontal: 5),
-                                        child: Text(
-                                          "第${e['sort']}章 ${e['name']}",
-                                          style: const TextStyle(
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.bold),
+            _AutoSuitableScrollView(
+                context: ctx,
+                child: Obx(() => Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: _classController.chaptersList
+                          .map((e) => Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 10),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 15, horizontal: 5),
+                                    child: Text(
+                                      "第${e['sort']}章 ${e['name']}",
+                                      style: const TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  Column(
+                                    children: e['chapterDetailEntityList']
+                                        .map((f) => Theme(
+                                            data: Theme.of(context).copyWith(
+                                                dividerColor:
+                                                    Colors.transparent),
+                                            child: ExpansionTile(
+                                              title: Text(
+                                                  "${e['sort']}.${f['sort']} ${f['name']}"),
+                                              children: f['teachContentResList']
+                                                  .map((g) => ListTile(
+                                                        onTap: () async {
+                                                          await _classController
+                                                              .contentResHandler(
+                                                                  g);
+                                                        },
+                                                        leading: Image.asset(
+                                                          g['type'] == 5
+                                                              ? extToIcons(
+                                                                  g['typeStr'])
+                                                              : type2Icons(g[
+                                                                      'type']
+                                                                  .toString()),
+                                                          height: 24,
+                                                        ),
+                                                        title: Text(
+                                                            "${g['name']}"),
+                                                      ))
+                                                  .cast<Widget>()
+                                                  .toList(),
+                                            )))
+                                        .cast<Widget>()
+                                        .toList(),
+                                  )
+                                ],
+                              )))
+                          .toList(),
+                    ))),
+            _AutoSuitableScrollView(
+                context: ctx,
+                child: Obx(() => Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: _classController.activitiesList
+                          .map((e) => Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 10),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 15, horizontal: 5),
+                                    child: Text(
+                                      DateFormat('yyyy-MM-dd').format(
+                                          DateTime.fromMillisecondsSinceEpoch(
+                                              int.parse(e['time']))),
+                                      style: const TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  for (var i in e['data']) ...[
+                                    ListTile(
+                                        leading: Image.asset(
+                                          type2Icons(i['type'].toString()),
+                                          height: 24,
                                         ),
-                                      ),
-                                      Column(
-                                        children: e['chapterDetailEntityList']
-                                            .map((f) => Theme(
-                                                data: Theme.of(context)
-                                                    .copyWith(
-                                                        dividerColor:
-                                                            Colors.transparent),
-                                                child: ExpansionTile(
-                                                  title: Text(
-                                                      "${e['sort']}.${f['sort']} ${f['name']}"),
-                                                  children:
-                                                      f['teachContentResList']
-                                                          .map((g) => ListTile(
-                                                                onTap:
-                                                                    () async {
-                                                                  await _classController
-                                                                      .contentResHandler(
-                                                                          g);
-                                                                },
-                                                                leading:
-                                                                    Image.asset(
-                                                                  g['type'] == 5
-                                                                      ? extToIcons(g[
-                                                                          'typeStr'])
-                                                                      : type2Icons(
-                                                                          g['type']
-                                                                              .toString()),
-                                                                  height: 24,
-                                                                ),
-                                                                title: Text(
-                                                                    "${g['name']}"),
-                                                              ))
-                                                          .cast<Widget>()
-                                                          .toList(),
-                                                )))
-                                            .cast<Widget>()
-                                            .toList(),
-                                      )
-                                    ],
-                                  )))
-                              .toList(),
-                        )))
-              ],
-            ),
-            CustomScrollView(
-              physics: const NeverScrollableScrollPhysics(),
-              slivers: [
-                SliverOverlapInjector(
-                  handle: NestedScrollView.sliverOverlapAbsorberHandleFor(ctx),
-                ),
-                SliverToBoxAdapter(
-                    child: Obx(() => Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: _classController.activitiesList
-                              .map((e) => Padding(
-                                  padding: EdgeInsets.symmetric(horizontal: 10),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Padding(
-                                        padding: EdgeInsets.symmetric(
-                                            vertical: 15, horizontal: 5),
-                                        child: Text(
-                                          DateFormat('yyyy-MM-dd').format(
-                                              DateTime
-                                                  .fromMillisecondsSinceEpoch(
-                                                      int.parse(e['time']))),
-                                          style: const TextStyle(
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                      ),
-                                      for (var i in e['data']) ...[
-                                        ListTile(
-                                            leading: Image.asset(
-                                              type2Icons(i['type'].toString()),
-                                              height: 24,
-                                            ),
-                                            title: Text("${i['name']}"),
-                                            onTap: () async {
-                                              await _classController
-                                                  .contentResHandler(i);
-                                            })
-                                      ]
-                                    ],
-                                  )))
-                              .toList(),
-                        )))
-              ],
-            ),
+                                        title: Text("${i['name']}"),
+                                        onTap: () async {
+                                          await _classController
+                                              .contentResHandler(i);
+                                        })
+                                  ]
+                                ],
+                              )))
+                          .toList(),
+                    ))),
           ],
         );
       }),
     ));
+  }
+}
+
+class _AutoSuitableScrollView extends StatelessWidget {
+  const _AutoSuitableScrollView({required context, required child})
+      : _child = child,
+        ctx = context;
+  final Widget _child;
+  final BuildContext ctx;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      physics: const NeverScrollableScrollPhysics(),
+      slivers: [
+        SliverOverlapInjector(
+          handle: NestedScrollView.sliverOverlapAbsorberHandleFor(ctx),
+        ),
+        SliverToBoxAdapter(child: _child)
+      ],
+    );
   }
 }
